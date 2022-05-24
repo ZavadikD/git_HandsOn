c823820d88912913d4583276554d01dfd4944e57
ZavadikD <zavadskadaryna@gmail.com>
2022-05-24 19:00:18 +0200

    final befor excersice

--- seqClass.py
+++ seqClass.py
@@ -2,7 +2,7 @@
 
 import sys, re
 from argparse import ArgumentParser
-
+#provides parser for the input
 parser = ArgumentParser(description = 'Classify a sequence as DNA or RNA')
 parser.add_argument("-s", "--seq", type = str, required = True, help = "Input sequence")
 parser.add_argument("-m", "--motif", type = str, required = False, help = "Motif")
@@ -12,6 +12,8 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 args.seq = args.seq.upper()
+
+#identifies nucleic acid type
 if re.search('^[ACGTU]+$', args.seq):
     if re.search('T', args.seq):
         print ('The sequence is DNA')
@@ -22,6 +24,7 @@ if re.search('^[ACGTU]+$', args.seq):
 else:
     print ('The sequence is not DNA nor RNA')
 
+#looks for motifs
 if args.motif:
     args.motif = args.motif.upper()
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
