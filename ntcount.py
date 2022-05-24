673474bef2c320b35af64aeb53986554a13a4a55
ZavadikD <zavadskadaryna@gmail.com>
2022-05-24 20:27:02 +0200

    now the script to count percentage of each nucleotide is working nicely.

--- ntcount.py
+++ ntcount.py
@@ -1,17 +1,13 @@
-class dnaString (str): 
-    def __new__(self,s): 
-        return str.__new__(self,s.upper()) 
-    def length (self): 
-        return (len(self)) 
-    def getATCG (self,num_A,num_T,num_C,num_G): 
-        num_A = self.count("A") 
-        num_T = self.count("T") 
-        num_C = self.count ("C") 
-        num_G = self.count ("G") 
-        return ( (self.length(), num_A, num_T, num_G, num_C) ) 
+DNASeq = input("Enter a DNA sequence: ")
 
-    def printnum_A (self): 
-        print ("Adenine base content: {0}".format(self.count("A"))) 
+SeqLength = len(DNASeq)
 
-dna = input("Enter a dna sequence: ") 
-x=dnaString(dna) 
+print('Sequence Length:', SeqLength)
+
+BaseKey = list(set(DNASeq)) #creates a list from the unique characters in the DNASeq
+
+Dict = {}
+
+for char in BaseKey:
+	Dict[char] = 100*(DNASeq.count(char)/SeqLength)
+print(Dict)
